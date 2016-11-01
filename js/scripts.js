bf01d9e073fca7fd75f0d106b8b2c935dbf4e575
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-01 17:08:45 -0500

    got geolocation working

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,19 @@ var app = angular.module('localWeather', []);
 
 
 
-app.controller('weatherController', function ($scope) {
+app.controller('weatherController', function ($scope, $http) {
 
-    
+
+
+    $http.jsonp("https://freegeoip.net/json?callback=JSON_CALLBACK").
+        success(function (data) {
+            console.log(data);
+        }).
+        error(function (data) {
+            console.log("Request failed");
+        });
+
+
 });
 
 
