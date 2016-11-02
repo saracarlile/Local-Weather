2baa4a04f09c88d80390aa3f8b0d19d013ea344a
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-02 14:24:47 -0500

    working the api results

--- js/scripts-jquery.js
+++ js/scripts-jquery.js
@@ -4,10 +4,13 @@ $(document).ready(function() {
     woeid: '',
     unit: 'f',
     success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<img src="' + weather.image + '">';
       html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
       html += '<li class="currently">'+weather.currently+'</li>';
       html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+      console.log(weather);
   
       $("#weather").html(html);
     },
