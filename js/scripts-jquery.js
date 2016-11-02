8ab47243be6a679cdbe5daf9d01d0cce3433bf3e
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-02 16:14:44 -0500

    cleanup project switch from angular to jquery

--- js/scripts-jquery.js
+++ js/scripts-jquery.js
@@ -1,52 +1,39 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+  var city;
+  var state;
+  $.getJSON("https://freegeoip.net/json?callback=?", function (result) {
+    //response data are now in the result variable
+    $.each(result, function (key, val) {
+      if (key === "city") {
+        city = val;
+      }
+      if (key === "region_code") {
+        state = val;
+      }
+    });
+  }).done(function () {
+    $.simpleWeather({
+      location: city + ", " + state,
+      woeid: '',
+      unit: 'f',
+      success: function (weather) {
 
-var city;
-var state;
-$.getJSON("https://freegeoip.net/json?callback=?", function(result){
-   //response data are now in the result variable
-   $.each(result, function (key, val){
-     console.log( key + "," + val);
-     if(key === "city"){
-       console.log("CITY " + val);
-       city = val;
-     }
-     if(key === "region_code"){
-       console.log("state " + val);
-       state = val;
-     }
-   });
-}).done(function() {
-    console.log("RRRRRR " + city + ", " + state);
-  }).done(function(){
-    console.log("Can I do this?");
-     $.simpleWeather({
-    location: city + ", " + state,
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      
-      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<img src="' + weather.image + '">';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
-    //  console.log(weather);
-  
-      $("#weather").html(html);
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-  });
+        html = '<h2>' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+        html += '<img src="' + weather.image + '">';
+        html += '<ul><li>' + weather.city + ', ' + weather.region + '</li>';
+        html += '<li class="currently">' + weather.currently + '</li>';
+        html += '<li>' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
+        //  console.log(weather);
+
+        $("#weather").html(html);
+      },
+      error: function (error) {
+        $("#weather").html('<p>' + error + '</p>');
+      }
+    });
   });
 
 
 
-
-
-
-// http://stackoverflow.com/questions/5943630/basic-example-of-using-ajax-with-jsonp
-
- 
 });
