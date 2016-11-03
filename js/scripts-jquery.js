34165a0529e9db810b536b2aa376315b6e47772b
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-11-03 09:30:37 -0500

    feeding api results into DOM

--- js/scripts-jquery.js
+++ js/scripts-jquery.js
@@ -2,31 +2,30 @@ $(document).ready(function () {
 
   var city;
   var state;
-  $.getJSON("https://freegeoip.net/json?callback=?", function (result) {
+  $.getJSON('https://freegeoip.net/json?callback=?', function (result) {
     //response data are now in the result variable
     $.each(result, function (key, val) {
-      if (key === "city") {
+      if (key === 'city') {
         city = val;
       }
-      if (key === "region_code") {
+      if (key === 'region_code') {
         state = val;
       }
     });
   }).done(function () {
     $.simpleWeather({
-      location: city + ", " + state,
+      location: city + ', ' + state,
       woeid: '',
       unit: 'f',
       success: function (weather) {
+        $('#loading').remove();
+        var gridOne = $('#grid-one');
+        gridOne.append('<p><img src="' + weather.image + '"></p>').append( '<h2 class="fix">' + weather.city + ', ' + weather.region + '</h2>');
 
-        html = '<h2>' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
-        html += '<img src="' + weather.image + '">';
-        html += '<ul><li>' + weather.city + ', ' + weather.region + '</li>';
-        html += '<li class="currently">' + weather.currently + '</li>';
-        html += '<li>' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
+         var gridTwo = $('#grid-two');
+          gridTwo.append().append( '<h2>' + weather.temp + '&deg;' + weather.units.temp + '</h2>').append('<p>' +  weather.currently + '</p>').append('<p>' + weather.wind.direction + ' ' + weather.wind.speed + ' ' + weather.units.speed + '</p>');
+
         //  console.log(weather);
-
-        $("#weather").html(html);
       },
       error: function (error) {
         $("#weather").html('<p>' + error + '</p>');
